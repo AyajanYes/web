ec7bac5e0f9750f20d734f74f39b3cc14ee2c05c
AyajanYes <71542329+AyajanYes@users.noreply.github.com>
2021-04-17 11:07:41 +0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,5 +6,14 @@ sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
 
-sudo /etc/init.d/mysql restart
-mysql -u root -e "CREATE DATABASE qa CHARACTER SET utf8"
+sudo /etc/init.d/mysql start
+#sudo systemctl start mariadb
+#mysql -uroot -e "DROP DATABASE IF EXISTS askappdb;"
+#mysql -uroot -e "DROP USER 'askapp'@'localhost';" mysql
+mysql -uroot -e "CREATE DATABASE qa CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+mysql -uroot -e "CREATE USER 'root'@'localhost' IDENTIFIED BY 'passwrd123';"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON qa.* TO 'root'@'localhost';"
+mysql -uroot -e "FLUSH PRIVILEGES;"
+
+sudo python ~/web/ask/manage.py makemigrations qa
+sudo python ~/web/ask/manage.py migrate
